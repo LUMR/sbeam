dfeedd8aa4b512f5d1cdacc96811755992ed552e
LUMR <freedomyes@sina.com>
2018-02-27 22:06:12 +0800

    把xml配置文件改用java类实现

---
+++ src/main/java/com/lumr/sbeam/config/RootConfig.java
@@ -0,0 +1,88 @@
+package com.lumr.sbeam.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * Created by work on 2018/2/24.
+ *
+ * @author lumr
+ */
+@Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.lumr.sbeam.dao")
+@PropertySource(value = "classpath:db.properties")
+public class RootConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource(@Value("${url}") String url, @Value("${username}") String userName, @Value("${password}") String password) throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setMaxActive(10);
+        dataSource.setInitialSize(1);
+        dataSource.init();
+        return dataSource;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer configurer = new MapperScannerConfigurer();
+        configurer.setBasePackage("com.lumr.sbeam.dao");
+        configurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        return configurer;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource, org.apache.ibatis.session.Configuration configuration) {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setTypeAliasesPackage("com.lumr.bbs.vo");
+        bean.setConfiguration(configuration);
+        return bean;
+    }
+
+    @Bean
+    public org.apache.ibatis.session.Configuration configuration() {
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.getTypeAliasRegistry().registerAliases("com.lumr.sbeam.vo");
+        return configuration;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(ServletContext servletContext){
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver(servletContext);
+        resolver.setMaxUploadSize(5000000L);
+        return resolver;
+    }
+
+}
