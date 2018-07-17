2cf3bd1977d702372905e76961fce04402cf091b
lumr <freedomyes@sina.com>
2018-07-17 22:58:14 +0800

    jsp转换成html

--- src/main/java/com/lumr/sbeam/controller/LibraryController.java
+++ src/main/java/com/lumr/sbeam/controller/LibraryController.java
@@ -8,9 +8,9 @@ import com.lumr.sbeam.vo.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -29,7 +29,7 @@ public class LibraryController {
     @Autowired
     private LibraryDao libraryDao;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping(value = "")
     public String libraryView(String cid, String pid, String gameName, Model model) {
         if (gameName != null)
             gameName = "%" + gameName + "%";
@@ -41,7 +41,7 @@ public class LibraryController {
     }
 
 
-    @RequestMapping(value = "/game/{id}")
+    @GetMapping(value = "/game/{id}")
     public String gameDetails(@PathVariable String id, Model model) {
         Integer gid = parseInt(id);
         Game game = gameDao.getGame(new Game(gid));
