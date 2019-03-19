30ec63aaaaacdcfab10a888233759f932d26839d
lumr <freedomyes@sina.com>
2019-03-20 01:02:18 +0800

    前端改用vue,前后端分离初始化

---
+++ web/src/views/Demo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p>hello!<span>{{ time }}</span> </p>
+    <a href="/">回首页</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'what is This?',
+      time: new Date().toLocaleTimeString()
+    }
+  }
+}
+</script>
