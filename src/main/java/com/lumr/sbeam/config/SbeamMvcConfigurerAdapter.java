9eceab8073f7c64f1f31b4ac8c60fae5ba373098
lumr <freedomyes@sina.com>
2019-03-21 19:08:52 +0800

    开启允许跨域

--- src/main/java/com/lumr/sbeam/config/SbeamMvcConfigurerAdapter.java
+++ src/main/java/com/lumr/sbeam/config/SbeamMvcConfigurerAdapter.java
@@ -1,7 +1,9 @@
 package com.lumr.sbeam.config;
 
 import com.lumr.sbeam.interceptor.AuthInterceptor;
+import com.lumr.sbeam.interceptor.CROSInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,6 +19,13 @@ public class SbeamMvcConfigurerAdapter implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor())
                 .addPathPatterns("/admin/**", "/user/**").excludePathPatterns("/user/login", "/user/register", "/user/checkName");
+        registry.addInterceptor(new CROSInterceptor()).addPathPatterns("/**");
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+
 
 }
---
+++ src/main/java/com/lumr/sbeam/interceptor/CROSInterceptor.java
@@ -0,0 +1,20 @@
+package com.lumr.sbeam.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author lumr freedomyes@sina.com
+ * @since 2019-03-21
+ **/
+public class CROSInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+        response.setHeader("Access-Control-Allow-Origin","*");
+    }
+
+}
