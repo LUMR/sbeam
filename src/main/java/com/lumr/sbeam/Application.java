7d3fbec712ddced5a743debe4bb21c13e3886b91
lumr <freedomyes@sina.com>
2019-03-21 13:42:12 +0800

    转换成springboot

---
+++ src/main/java/com/lumr/sbeam/Application.java
@@ -0,0 +1,15 @@
+package com.lumr.sbeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lumr fslumr777@sina.com
+ * @since 2018/6/29
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- src/main/java/com/lumr/sbeam/vo/resp/BaseResp.java
+++ src/main/java/com/lumr/sbeam/vo/resp/BaseResp.java
@@ -1,19 +1,14 @@
 package com.lumr.sbeam.vo.resp;
 
 import com.lumr.sbeam.vo.ResponseCode;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Created by work on 2018/2/24.
  *
  * @author lumr
  */
-@ApiModel(description = "响应公共类")
 public class BaseResp {
-    @ApiModelProperty(notes = "状态码")
     private String code;
-    @ApiModelProperty(notes = "提示信息")
     private String message;
 
     public String getCode() {
